9de023e48ce8e26973e75d56e5a65973543e7a5a
AdelieC <40043440+AdelieC@users.noreply.github.com>
2021-06-02 11:31:37 +0200

    Deleted DAOFactory to be able to merbe with main

--- Swap/src/main/java/com/swap/dal/DAOFactory.java
+++
@@ -1,9 +0,0 @@
-package com.swap.dal;
-
-import com.swap.dal.jdbc.UserDAOjdbc;
-
-public class DAOFactory {
-	public static UserDAO getUserDAO() throws DALException {
-		return new UserDAOjdbc();
-	}
-}

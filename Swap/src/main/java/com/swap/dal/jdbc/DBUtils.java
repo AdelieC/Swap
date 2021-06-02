f390b9f7618a769eca5eb5ab907bb0290e5e7886
AdelieC <40043440+AdelieC@users.noreply.github.com>
2021-06-02 11:40:28 +0200

    Updated DBUtils to be able to merge User branch

--- Swap/src/main/java/com/swap/dal/jdbc/DBUtils.java
+++ Swap/src/main/java/com/swap/dal/jdbc/DBUtils.java
@@ -20,6 +20,13 @@ public class DBUtils {
 	public static String searchBy(String tableName, String col) {
 		return "SELECT * FROM " + tableName + " WHERE " + col + " LIKE ?";
 	}
+	
+	public static String findExactMatchIn(String tableName, String[] columns) {
+		String matches = columns[0] + " = ?";
+		for (int i = 1; i < columns.length; i++)
+			matches += (" AND " + columns[i] + " = ?");
+		return "SELECT " + columns[0] + " FROM " + tableName + " WHERE " + matches;
+	}
 
 	public static String twoCriteriasSearch(String tableName, String col1, String col2) {
 		return "SELECT * FROM " + tableName + " WHERE " + col1 + " LIKE ? OR " + col2 + " LIKE ?";
@@ -43,5 +50,25 @@ public class DBUtils {
 
 	public static String deleteWhere(String tableName, String col) {
 		return "DELETE FROM " + tableName + " WHERE " + col + " = ?";
+	}
+	
+	public static void closeConnection(Connection conn) throws DALException {
+		if (conn != null) {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				throw new DALException("Failed to close connection - ", e);
+			}
+			conn = null;
+		}
+	}
+
+	public static void closePrepStmt(PreparedStatement rqt) throws DALException {
+		try {
+			if (rqt != null)
+				rqt.close();
+		} catch (SQLException e) {
+			throw new DALException("Failed to close prepared statement - ", e);
+		}
 	}
 }
